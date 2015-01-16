86fb8c569959250e0b63f92938851dc69d7e53af
fonglh <fonglh@gmail.com>
2015-01-16 17:18:30 +0800

    Try out single table inheritance

---
+++ app/models/image_url.rb
@@ -0,0 +1,3 @@
+#inherit from Url, so it also inherits from ActiveRecord base
+class ImageUrl < Url
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116033325) do
+ActiveRecord::Schema.define(version: 20150116084633) do
 
   create_table "urls", force: :cascade do |t|
     t.text     "long_url"
     t.string   "short_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "type"
   end
 
 end
